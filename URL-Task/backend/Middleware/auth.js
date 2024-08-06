57b1131dcf442beaa7acd465baca1dc7b5c77527
KARTHICK-R <karthicknandha619@gmail.com>
2024-08-06 19:24:53 +0530

    updated

--- URL-Task/backend/Middleware/auth.js
+++ URL-Task/backend/Middleware/auth.js
@@ -1,17 +1,18 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-module.exports = function(req,res,next) {
+module.exports = function (req, res, next) {
     const token = req.headers.authorization;
-    if(!token){
-        res.status(400).send("No Token Not Authorized!")
+    if (!token) {
+        return res.status(401).send("Access Denied. No token provided!");
     }
-    try{
+    
+    try {
         const verifyToken = token.split(" ")[1];
-        const user = jwt.verify(verifyToken,process.env.JWTSECRET)
+        const user = jwt.verify(verifyToken, process.env.JWTSECRET);
         req.user = user._id;
         next();
     } catch (err) {
-        res.status(400).send("Invalid Token")
+        res.status(400).send("Invalid Token");
     }
-}
+};
